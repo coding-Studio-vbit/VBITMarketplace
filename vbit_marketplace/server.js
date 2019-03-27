dff0d67203bcff36e5398ba672b335d5e7a9ad48
Yashasvi Mutteneni <yashincontrol@gmail.com>
2019-03-27 22:40:54 +0530

    Adding email verification

--- vbit_marketplace/server.js
+++ vbit_marketplace/server.js
@@ -1,11 +1,15 @@
 const accountSid = 'ACe7faaeafde1aafc4c694f3b68c1035bc';
 const authToken = 'fc81ae82e6c7469051947bc5c72a6225';
 const client = require('twilio')(accountSid, authToken);
+const nodemailer = require('nodemailer');
 var request = require('request');
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var randomInt = require('random-int');
+
+app.use(bodyParser.json());
+
 //initializing knex
 var knex = require('knex')({
   client: 'pg',
@@ -50,6 +54,46 @@ app.get('verifyOTP',function(req,res){
         }
     })
 });
+
+
+app.get('/email', (req,res) => {
+    var uid = req.body.uid;
+    sendEmail(uid);
+});
+
+function sendEmail(uid){
+     let rand = Math.floor((Math.random()*100) + 54);
+     knex('users').where({
+        uid: uid
+     }).insert({userverificationcode: rand})
+    link = "http://localhost:3000"+"/verifyemail?id="+rand;
+    const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'nathanial84@ethereal.email',
+        pass: '7t8uWSbShsb8wFwSNu'
+    }
+});
+var mailOptions = {
+  from: 'Sender Name <sender@example.com>',
+  to: 'Recipient <recipient@example.com>',
+  subject: 'Confirm your email account',
+  html: "<a href="+link+">click here to verify</a>"
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+
+app.get('/verify', function(req,res){
+    
+
+})    
 // listen for all incoming requests
 app.listen(3000, function(){
   console.log("Server is listening on port 3000");

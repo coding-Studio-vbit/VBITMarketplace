ba26dad6c9e50256c532fd165348605f5e72c861
msaichaitanya735 <43997513+msaichaitanya735@users.noreply.github.com>
2019-03-30 15:07:04 +0530

    First Commit

--- vbit_marketplace_fe/src/App.js
+++ vbit_marketplace_fe/src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './style.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <nav>
+        <h1 className="VBIT"><a href="#">VBI<span>T</span></a></h1>
+          <div className='main'>
+            <ul>
+              <li><a href='#'>Home</a></li>
+              <li><a href='#'>WishList</a></li>
+              <li><a href='#'>About</a></li>
+              <div className='search-box'>
+              <input type="text" name=""placeholder="Type to search"/>
+              <a className="search-btn" href="#"></a>
+              <i className="fa-search"></i>
+              </div>
+              
+            </ul>
+          </div>
+         
+          
+        
+      </nav>
+    </div>
     );
   }
 }
